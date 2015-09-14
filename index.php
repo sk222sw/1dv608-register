dcf44587a50dcfc0f17b56e4fdfc1effad3e376e
Sonny Kjellberg <sk222sw@student.lnu.se>
2015-09-14 11:13:53 +0000

    heroku test

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 
+echo 'hej';
+
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
